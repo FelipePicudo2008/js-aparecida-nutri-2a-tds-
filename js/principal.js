27df375cf10b739ab35a7bfc85f77328c485a838
FelipePicudo2008 <161484527+FelipePicudo2008@users.noreply.github.com>
2024-10-04 08:43:44 -0300

    Atualização_v1

--- js/principal.js
+++ js/principal.js
@@ -38,10 +38,4 @@ if (alturaEhValida && pesoEhValido) {
     tdImc.textContent = "Altura e/ou peso inválidos!"
 }
 
-/*<tr class="paciente" id="primeiro-paciente">
-<td class="info-nome">Paulo</td>
-<td class="info-peso">100</td>
-<td class="info-altura">2.00</td>
-<td class="info-gordura">10</td>
-<td class="info-imc">0</td>*/
 
